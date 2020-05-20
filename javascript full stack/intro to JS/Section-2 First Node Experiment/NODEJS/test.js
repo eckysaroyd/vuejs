c593a53729f7b7030e4968f40442866157042812
eckysaroyd <enyato98@gmail.com>
2020-05-20 07:11:41 +0530

    after install nodeJS

---
+++ javascript full stack/intro to JS/Section-2 First Node Experiment/NODEJS/test.js
@@ -0,0 +1,11 @@
+    let strowberryCount = 2
+
+if( strowberryCount >10 ) 
+{
+    console.log("You have Enough strowberryCount")
+}
+else
+{
+    console.log("You should not have Enough strowberryCount")
+
+}
