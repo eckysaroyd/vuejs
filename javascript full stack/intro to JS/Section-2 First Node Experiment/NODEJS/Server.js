fe2d4b2c0bc8a39199641a703a14c20f689431e9
eckysaroyd <enyato98@gmail.com>
2020-05-20 07:39:34 +0530

    new data has added here

--- javascript full stack/intro to JS/Section-2 First Node Experiment/NODEJS/Server.js
+++ javascript full stack/intro to JS/Section-2 First Node Experiment/NODEJS/Server.js
@@ -2,7 +2,13 @@ let http = require("http")
 
 let ourApp = http.createServer(function(req,res){
 
-    console.log(req.url)
-    res.end("Hello welcome to our website")
+    //console.log(req.url)
+    if(req.url=="/"){
+        res.end("Hello welcome to our Homepage")
+    }
+    if(req.url=="/about"){
+        res.end("thank you for visiting our Page")
+    }
+    res.end("We are not able to find your request")
 })
 ourApp.listen(3000)
