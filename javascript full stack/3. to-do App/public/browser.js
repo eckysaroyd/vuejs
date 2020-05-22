8dde5df586d2cb155fca812aac4685218e0e4dab
eckysaroyd <enyato98@gmail.com>
2020-05-23 04:07:06 +0530

    new data has been added

--- javascript full stack/3. to-do App/public/browser.js
+++ javascript full stack/3. to-do App/public/browser.js
@@ -2,14 +2,18 @@ document.addEventListener("click",function(e){
  
     if (e.target.classList.contains("edit-me"))
     {
-     let userinput =  prompt("Enter your desire new Test")
+     let userinput =  prompt("Enter your desire new Test",e.target.parentElement.parentElement.querySelector(".item-text").innerHTML)
     
      //console.log(userinput)
-     axios.post('/update-item',{text : userinput, id : e.target.getAttribute("data-id")}).then(function(){
-         //do something interest here in next video
-     }).catch(function(){
-         console.log("pleasw try again leter")
-     })
+     if(userinput){
+        axios.post('/update-item',{text : userinput, id : e.target.getAttribute("data-id")}).then(function(){
+            //do something interest here in next video
+           e.target.parentElement.parentElement.querySelector(".item-text").innerHTML = userinput
+           }).catch(function(){
+            console.log("pleasw try again leter")
+        })
+         
+     }
     }
 
 
