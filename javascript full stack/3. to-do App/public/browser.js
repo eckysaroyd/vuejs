3ec2529ddd0455daab9c3c5d3e79863048a84aa4
eckysaroyd <enyato98@gmail.com>
2020-05-23 04:40:35 +0530

    delete option

--- javascript full stack/3. to-do App/public/browser.js
+++ javascript full stack/3. to-do App/public/browser.js
@@ -1,5 +1,19 @@
 document.addEventListener("click",function(e){
- 
+    //delete Feature
+    if (e.target.classList.contains("delete-me"))
+    {
+        if(confirm("Do you Really want to delete this Permanent....?")){
+            axios.post('/delete-item',{ id : e.target.getAttribute("data-id")}).then(function(){
+                //do something interest here in next video
+               e.target.parentElement.parentElement.remove()
+               }).catch(function(){
+                console.log("please try again leter")
+            })
+        }
+
+    }
+    
+    //update feature
     if (e.target.classList.contains("edit-me"))
     {
      let userinput =  prompt("Enter your desire new Test",e.target.parentElement.parentElement.querySelector(".item-text").innerHTML)
--- javascript full stack/3. to-do App/server.js
+++ javascript full stack/3. to-do App/server.js
@@ -52,7 +52,7 @@ app.get('/', function(req, res)
           <span class="item-text">${item.text}</span>
           <div>
             <button data-id ="${item._id}" class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
-            <button class="delete-me btn btn-danger btn-sm">Delete</button>
+            <button data-id ="${item._id}"  class="delete-me btn btn-danger btn-sm">Delete</button>
           </div>
         </li>`
        }).join("")
@@ -88,6 +88,11 @@ db.collection('items').findOneAndUpdate({_id: new mongodb.ObjectID(req.body.id)}
 })
 }) 
 
+app.post('/delete-item', function(req,res){
+    db.collection('items').deleteOne({_id: new mongodb.ObjectID(req.body.id)},{$set : {text:req.body.text}},function(){
+        res.send("succsess")
+    })  
+})
 
 
 //nodemon - node start your app, and mon is monetary your app
