175732112a474679d797678d33e9935e0cb73708
eckysaroyd <enyato98@gmail.com>
2020-06-14 09:10:30 +0530

    added article

---
+++ javascript full stack/4. articlePost/controllers/userController.js
@@ -0,0 +1,16 @@
+exports.login = function ()
+{
+
+}
+exports.logout = function ()
+{
+    
+}
+exports.register = function ()
+{
+    
+}
+exports.home = function (req,res)
+{
+    res.render('home-guest')
+}
--- javascript full stack/4. articles/router.js
+++
@@ -1,2 +0,0 @@
-console.log("I'm executed immediately")
-module.exports = "I am the export for the router file"
