2d07096a32e22681abb3fb38890ed26083c33eb9
eckysaroyd <enyato98@gmail.com>
2020-06-14 22:33:03 +0530

    finish validation method

--- javascript full stack/4. articlePost/models/User.js
+++ javascript full stack/4. articlePost/models/User.js
@@ -1,4 +1,6 @@
-let User = function(data)
+    const validator = require("validator")
+
+    let User = function(data)
     {
         this.data = data
         this.errors = []
@@ -6,9 +8,14 @@ let User = function(data)
     User.prototype.validate =function()
     {
         if(this.data.username == "") {this.errors.push("You must provide a  user name")}
-        if(this.data.email == "") {this.errors.push("You must provide a valid Email Address")}
+        if(this.data.username !="" && !validator.isAlphanumeric(this.data.username)){this.errors.push("Username can only contain letters and numbers")}
+        if(!validator.isEmail(this.data.email)) {this.errors.push("You must provide a valid Email Address")}
         if(this.data.password == "") {this.errors.push("You must provide a password")}
-
+        if(this.data.password.length> 0 && this.data.password.length <12 ) {this.errors.push("Your password must be atleast 12 characters")}
+        if(this.data.password.length> 100) {this.errors.push("Passoword can not exceed 100 characters")}
+        if(this.data.username.length> 0 && this.data.username.length <3 ) {this.errors.push("Your Username must be atleast 3 characters")}
+        if(this.data.username.length> 30) {this.errors.push("Username can not exceed 30 characters")}
+       
     }
 
     User.prototype.register = function(){
