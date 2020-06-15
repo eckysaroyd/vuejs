4295b966c88495603adb53c735082436ecec27e7
eckysaroyd <enyato98@gmail.com>
2020-06-15 15:05:24 +0530

    finish adding data to the dataBase

--- javascript full stack/4. articlePost/models/User.js
+++ javascript full stack/4. articlePost/models/User.js
@@ -1,3 +1,4 @@
+    const usersCollection =  require("../db").collection("users")
     const validator = require("validator")
 
     let User = function(data)
@@ -38,6 +39,9 @@
         this.cleanUp()
         this.validate()
     //#step 2 : Only if there are no validation errors,Then save the user data in to database
-
+        if(!this.errors.length)
+        {
+            usersCollection.insertOne(this.data)
+        }
     }
 module.exports = User
