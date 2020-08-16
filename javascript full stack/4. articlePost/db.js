d9636ec271c26aac7468d0aa1b9eb010e3ee5387
eckysaroyd <enyato98@gmail.com>
2020-08-17 02:45:35 +0530

    new added dara

--- javascript full stack/4. articlePost/db.js
+++
@@ -1,9 +0,0 @@
-const dotenv = require('dotenv')
-const mongodb = require("mongodb")
-dotenv.config()
-mongodb.connect(process.env.CONNECTIONSTRING,{ useNewUrlParser:true, useUnifiedTopology:true }, function(err,client){
-
-    module.exports = client.db()
-    const app = require ("./app")
-    app.listen(process.env.PORT)
-})
