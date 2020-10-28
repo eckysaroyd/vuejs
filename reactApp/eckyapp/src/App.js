6a20f98431269de2f92d561e47d2c595ab2e45de
eckysaroyd <enyato98@gmail.com>
2020-10-29 03:32:39 +0530

    a beggining of react course

---
+++ reactApp/eckyapp/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+var hello ="Welcome to the road to learn react";
+function App() {
+  return (
+    <div className="App">
+    <h2>{hello}</h2>
+    </div>
+  );
+}
+
+export default App;
