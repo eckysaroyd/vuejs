0e2339c3782ec8639db0862c4d3782ba1d8a2e3a
eckysaroyd <enyato98@gmail.com>
2020-12-30 02:39:48 +0530

    debug

---
+++ CHROME EXTENSIONS/Budget Manager/options.js
@@ -0,0 +1,19 @@
+$(function(){
+
+chrome.storage.sync.get('limit',function(budget){
+	$('#limit').val(budget.limit);
+})
+
+$('#saveLimit').click(function(){
+	var limit = $('#limit').val();
+	if(limit)
+	{
+		chrome.storage.sync.set({'limit':limit},function(){
+			close();
+		});
+	}
+});
+$('#resetLimit').click(function(){
+	chrome.storage.sync.set({'total':0});
+	});
+});
--- CHROME EXTENSIONS/Budget Manager/popup.js
+++ CHROME EXTENSIONS/Budget Manager/popup.js
@@ -1,6 +1,12 @@
 $(function(){
+
+chrome.storage.sync.get(['total','limit'], function(budget){
+	$('#total').text(budget.total);
+	$('#limit').text(budget.limit);
+})
+
 	$("#spendAmount").click(function(){
-		chrome.storage.sync.get('total', function(budget){
+		chrome.storage.sync.get(['total','limit'], function(budget){
 			var newTotal = 0;
 			if(budget.total){
 				newTotal += parseInt(budget.total);
@@ -9,7 +15,17 @@ $(function(){
 			if(amount){
 				newTotal += parseInt(amount);
 			}
-			chrome.storage.sync.set({'total':newTotal});
+			chrome.storage.sync.set({'total':newTotal}, function(){
+				if( amount && newTotal >= budget.limit){
+					var notfOptions = {
+						type:'basic',
+						iconUrl:'icon48.png',
+						title:'Limit Reached!',
+						message : "Sorry,You have reached the limit"
+					};
+					chrome.notifications.create('limitNotif',notfOptions);
+				}
+			});
 
 			$('#total').text(newTotal);
 			$('#amount').val('');
