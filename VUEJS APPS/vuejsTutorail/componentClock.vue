3a12a552bc9265dd6b785a3601fb31504b55d079
eckysaroyd <enyato98@gmail.com>
2022-10-12 22:04:38 +0530

    complete sample app

---
+++ VUEJS APPS/vuejsTutorail/componentClock.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="clock">{{ now }} </div>
+</template>
+<script>
+    export default {
+        data(){
+        return {
+            message:'Time Format',
+            now : new Date()
+        }
+    },
+    methods:{
+        UpdateDateTime(){
+            // console.log("update me now"); 
+            // this.now =new Date();
+            const d =
+            this.format==="short" ? new Date().toLocaleTimeString() : new Date();
+            this.now=d;
+            console.log(d);
+
+        }
+    },
+    mounted(){
+        setInterval(()=>{
+            this.UpdateDateTime();
+        },1000);
+    }
+    };
+</script>
+<style>
+.clock{
+    background-color: black;
+    color: cornsilk;
+}
+</style>
